b8b104f33b4b90c4b568e69dd65d9f1e0cc8fff4
zhou411424 <zhou411424@gmail.com>
2017-11-18 18:52:09 +0800

    网络检查，加载重试

--- app/src/main/java/com/chuanshi/pos/MainActivity.java
+++ app/src/main/java/com/chuanshi/pos/MainActivity.java
@@ -2,13 +2,18 @@ package com.chuanshi.pos;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
+import com.chuanshi.pos.library.http.NetworkUtil;
 import com.chuanshi.pos.widget.CustomWebView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     private CustomWebView mWebView;
+    private LinearLayout mNetworkErrorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,27 +23,50 @@ public class MainActivity extends Activity {
                 WindowManager.LayoutParams. FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
-        mWebView = (CustomWebView) findViewById(R.id.webview);
+        mWebView = findViewById(R.id.webview);
         //此句加上也可以，防止输入法调不出来
         mWebView.requestFocusFromTouch();
-
-        String url = "http://fwy.csshidai.com";
-        mWebView.loadUrl(url);
+        mNetworkErrorLayout = findViewById(R.id.layout_network_error);
+        Button reloadBtn = findViewById(R.id.btn_reload);
+        reloadBtn.setOnClickListener(this);
+        loadData();
     }
 
+    private void loadData() {
+        //网络检查
+        if (!NetworkUtil.isNetworkAvailable(this)) {
+            mWebView.setVisibility(View.GONE);
+            mNetworkErrorLayout.setVisibility(View.VISIBLE);
+        } else {
+            mWebView.setVisibility(View.VISIBLE);
+            mNetworkErrorLayout.setVisibility(View.GONE);
+            String url = "http://fwy.csshidai.com";
+            mWebView.loadUrl(url);
+        }
+    }
+    
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_reload:
+                loadData();
+                break;
+        }
+    }
+    
     @Override
     protected void onPause() {
-        if (mWebView != null) {
+        /*if (mWebView != null) {
             mWebView.onPause();
-        }
+        }*/
         super.onPause();
     }
 
     @Override
     protected void onResume() {
-        if (mWebView != null) {
+        /*if (mWebView != null) {
             mWebView.onResume();
-        }
+        }*/
         super.onResume();
     }
 
@@ -50,4 +78,5 @@ public class MainActivity extends Activity {
         }
         super.onDestroy();
     }
+    
 }
